3d0e34bb8ccc41a577822beb24592c8f097fe302
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2025-04-04 20:18:10 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [{ path: '', component: NavbarComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
